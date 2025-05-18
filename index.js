5663f66150aa77b494e42e19b366c16206005867
NisanXnx <brandofnisan198@gmail.com>
2025-05-18 12:02:24 +0600

    Update index.js

--- index.js
+++ index.js
@@ -36,3 +36,29 @@ function startProject() {
 }
 
 startProject();
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 8080; 
+
+app.get("/", (req, res) => {
+    res.send("Messenger bot is running!");
+});
+
+app.get("/webhook", (req, res) => {
+    let VERIFY_TOKEN = "your_verify_token"; 
+
+    let mode = req.query["hub.mode"];
+    let token = req.query["hub.verify_token"];
+    let challenge = req.query["hub.challenge"];
+
+    if (mode && token === VERIFY_TOKEN) {
+        res.status(200).send(challenge);
+    } else {
+        res.sendStatus(403);
+    }
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server is running on port ${PORT}`);
+});
